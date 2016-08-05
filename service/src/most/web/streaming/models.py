f2d2bca41f56b9bbc42ef4f32c88b600494c63e3
Vittorio Meloni <vittorio.meloni@crs4.it>
2016-08-05 08:20:33 +0200

    Changed StreamingDevice model to always return info about all streams

--- service/src/most/web/streaming/models.py
+++ service/src/most/web/streaming/models.py
@@ -87,14 +87,10 @@ class StreamingDevice(models.Model):
     def _get_json_dict(self):
 
         capabilities = {}
-        if self.model.has_streaming:
-            capabilities['streaming'] = self._get_streaming_url()
-        if self.model.has_web:
-            capabilities['web'] = self._get_web_url()
-        if self.model.has_streaming:
-            capabilities['shot'] = self._get_shot_url()
-        if self.model.has_streaming:
-            capabilities['ptz'] = self._get_ptz_url()
+        capabilities['streaming'] = self._get_streaming_url()
+        capabilities['web'] = self._get_web_url()
+        capabilities['shot'] = self._get_shot_url()
+        capabilities['ptz'] = self._get_ptz_url()
 
         return {
             'uuid': self.uuid,
